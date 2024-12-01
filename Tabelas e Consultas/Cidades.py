414c9808a7477e28b62a890cf9a41de224a5aa87
Bruno <bruno.moreira@aprendacerto.com.br>
2024-12-01 12:46:13 -0300

    Commit final.

--- Tabelas e Consultas/Cidades.py
+++ Tabelas e Consultas/Cidades.py
@@ -8,11 +8,17 @@ if "conn" in st.session_state and st.session_state.conn:
         ORDER BY Nome_Escola;
         """
         
+        st.write("Testando conexão com o banco.")
+        pd.read_sql("SELECT 1", con=st.session_state.conn)
+        st.success("Conexão bem-sucedida.")
+        
         df = pd.read_sql(query, con=st.session_state.conn)
 
-        # Exibe o dataframe com os resultados
-        st.title("Total de Alunos por Nível de Ensino")
-        st.dataframe(df)
+        if df.empty:
+            st.warning("Nenhum dado encontrado na tabela `resumo_alunos_escola`.")
+        else:
+            st.title("Total de Alunos por Nível de Ensino")
+            st.dataframe(df)
 
     except Exception as e:
         st.error(f"Erro ao buscar dados: {e}")
